407384d1d0be0c7251275f1785f007fa7aadce04
Михаил Москалев <miiixsc@gmail.com>
2018-01-29 21:38:20 +0300

    *_*

--- src/js/main.js
+++ src/js/main.js
@@ -4,13 +4,21 @@ var app = new Vue({
     el: '#app',
     data: {
         todoInputValue: '',
-        todoList: [],
-        taskText: ''
+        taskText: '',
+        todos: ['1', '2']
+    },
+    components: {
+        todol: {
+            data: function() {
+                return {todos}
+            },
+            template: '<ul class="todo__list"><li v-for="todo in todos" class="todo__item">{{ todo }}</li></ul>'
+        }
     },
     methods: {
         addTask: function() {
             this.taskText = event.target.value;
-            this.todoList.push(this.taskText);
+            this.todos.push(this.taskText);
         }
     }
 })
