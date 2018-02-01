e3dc0fa5a98e4fefe1b036bfb2d35180b300d039
mosk <miiixsc@gmail.com>
2018-02-01 16:52:27 +0300

    добавил кнопку

--- src/js/main.js
+++ src/js/main.js
@@ -5,20 +5,16 @@ var app = new Vue({
     data: {
         todoInputValue: '',
         taskText: '',
-        todos: ['1', '2']
-    },
-    components: {
-        todol: {
-            data: function() {
-                return {todos}
-            },
-            template: '<ul class="todo__list"><li v-for="todo in todos" class="todo__item">{{ todo }}</li></ul>'
-        }
+        todos: []
     },
     methods: {
         addTask: function() {
             this.taskText = event.target.value;
             this.todos.push(this.taskText);
+        },
+        deleteTask: function() {
+
         }
-    }
+    },
+    template: '<main class="todo"><input v-on:change="addTask" type="text" class="todo__input" placeholder="What needs to be done?" v-bind:value="todoInputValue"><ul class="todo__list"><li v-for="todo in todos" class="todo__item">{{ todo }}<button class="button button--delete">Удалить</button></li></ul></main>'
 })
